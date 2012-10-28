9e912cab8b422affb777612e8490db3ed28a8cdf
magnet88jp <magnet88jp@gmail.com>
2012-10-28 13:56:13 +0900

    s3 upload

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 import play.*;
 import play.mvc.*;
 
-import java.util.*;
+import play.libs.MimeTypes;
+import play.modules.s3blobs.S3Blob;
 
 import models.*;
 
@@ -13,4 +18,34 @@ public class Application extends Controller {
         render();
     }
 
+  public static void uploadForm()
+  {
+    render();
+  }
+
+  public static void doUpload(File file, String comment) throws FileNotFoundException
+  {
+    final Document doc = new Document();
+    doc.fileName = file.getName();
+    doc.comment = comment;
+    doc.file = new S3Blob();
+    doc.file.set(new FileInputStream(file), MimeTypes.getContentType(file.getName()));
+    
+    doc.save();
+    listUploads();
+  }
+
+  public static void listUploads()
+  {
+    List<Document> docs = Document.findAll();
+    render(docs);
+  }
+
+  public static void downloadFile(long id)
+  {
+    final Document doc = Document.findById(id);
+    notFoundIfNull(doc);
+    response.setContentTypeIfNotSet(doc.file.type());
+    renderBinary(doc.file.get(), doc.fileName);
+  }
 }
